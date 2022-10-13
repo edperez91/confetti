8b7b07b82c678d30b6a1ee4a019bc650617449c0
edperez91 <103350127+edperez91@users.noreply.github.com>
2022-10-13 11:44:05 -0700

    Added Lettering JS
    Made odd letters blueviolet

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ let confetti = new Confetti("confetti");
 
 // Optionalize it
 confetti.destroyTarget(false);
+
+// Run Lettering
+var lettering = new Lettering(".header");
+
+// Options
+lettering.letters();
